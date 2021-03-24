0ff62cb80da39c947bd644d4c56ab36b5ab3e0a4
Eric Liu <erichyliu08@gmail.com>
2021-03-24 02:17:00 -0400

    created test input file

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ def main():
         print("You must pass an input file as the argument", file = sys.stderr)
         return
     try:
-        inputFile = open(sys.argv[1])
+        with open(sys.argv[1], 'r') as inputFile:
+            line = inputFile.read().splitlines()
+            print(line)
     except FileNotFoundError:
         print("Input file %s does not exist" % sys.argv[1])
 
