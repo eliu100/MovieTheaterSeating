7c4fb066ea31c412386bd207f11160732a956cc7
Eric Liu <erichyliu08@gmail.com>
2021-03-24 02:04:26 -0400

    added basic i/o

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+import sys
 import theater
+
+def main():
+    if (len(sys.argv) != 2):
+        print("You must pass an input file as the argument", file = sys.stderr)
+        return
+    try:
+        inputFile = open(sys.argv[1])
+    except FileNotFoundError:
+        print("Input file %s does not exist" % sys.argv[1])
+
+
+
+if __name__ == "__main__":
+    main()
